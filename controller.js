645e41d589da07fd8fc71dcb9e03f0a21c94dc2d
fatahmsili <fatahmsili@gmail.com>
2019-05-14 11:58:24 +0200

    telechargement sous fichier json

--- controller.js
+++
@@ -1,8 +0,0 @@
-$( "form" ).submit(function( event ) {
-    var oElements = {};
-    $('form [name]').each(function(){
-        oElements[this.name] = this.value;
-    });
-    console.log(oElements);
-    event.preventDefault();
-});
